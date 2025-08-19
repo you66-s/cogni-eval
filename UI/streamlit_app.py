aa57b472085cf18d9a6d9eff5f9913917d969dd1
youssef <youssef@email.com>
2025-08-19 13:30:35 -0700

    intiate the structure of the ui

---
+++ UI/streamlit_app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+# pages definition
+main_page = st.Page("main_page.py", title="Main Page")
+logic_quiz_page = st.Page("logic_quiz_page.py", title="Logic Quiz Page")
+deduction_quiz_page = st.Page("deduction_quiz_page.py", title="Deduction & Induction Quiz Page")
+culture_quiz_page = st.Page("culture_quiz_page.py", title="General Culture Quiz Page")
+creativty_quiz_page = st.Page("creativity_quiz_page.py", title="Culture Quiz Page")
+reasoning_quiz_page = st.Page("Reasoning_quiz_page.py", title="Reasoning Quiz Page")
+technical_quiz_page = st.Page("technical_quiz_page.py", title="Technical Quiz Page")
+quiz_results = st.Page("quiz_results.py", title="Quiz Results")
+
+pages = st.navigation([main_page, logic_quiz_page, deduction_quiz_page, culture_quiz_page, creativty_quiz_page, reasoning_quiz_page, technical_quiz_page, quiz_results])
+pages.run()
