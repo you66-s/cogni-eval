f9aa0c1a89add9b9eafaa2d61ed5fe297c221e35
youssef <youssef@email.com>
2025-08-20 18:36:06 -0700

    defined the main structure of the app

--- UI/streamlit_app.py
+++ UI/streamlit_app.py
@@ -2,13 +2,14 @@ import streamlit as st
 
 # pages definition
 main_page = st.Page("main_page.py", title="Main Page")
-logic_quiz_page = st.Page("logic_quiz_page.py", title="Logic Quiz Page")
-deduction_quiz_page = st.Page("deduction_quiz_page.py", title="Deduction & Induction Quiz Page")
-culture_quiz_page = st.Page("culture_quiz_page.py", title="General Culture Quiz Page")
-creativty_quiz_page = st.Page("creativity_quiz_page.py", title="Culture Quiz Page")
-reasoning_quiz_page = st.Page("Reasoning_quiz_page.py", title="Reasoning Quiz Page")
-technical_quiz_page = st.Page("technical_quiz_page.py", title="Technical Quiz Page")
-quiz_results = st.Page("quiz_results.py", title="Quiz Results")
+logic_quiz_page = st.Page("pages/logic_quiz_page.py", title="Logic Quiz Page")
+deduction_quiz_page = st.Page("pages/deduction_quiz_page.py", title="Deduction & Induction Quiz Page")
+culture_quiz_page = st.Page("pages/culture_quiz_page.py", title="General Culture Quiz Page")
+creativty_quiz_page = st.Page("pages/creativity_quiz_page.py", title="Culture Quiz Page")
+reasoning_quiz_page = st.Page("pages/Reasoning_quiz_page.py", title="Reasoning Quiz Page")
+technical_quiz_page = st.Page("pages/technical_quiz_page.py", title="Technical Quiz Page")
+quiz_results = st.Page("pages/quiz_results.py", title="Quiz Results")
 
-pages = st.navigation([main_page, logic_quiz_page, deduction_quiz_page, culture_quiz_page, creativty_quiz_page, reasoning_quiz_page, technical_quiz_page, quiz_results])
+pages = st.navigation([main_page, logic_quiz_page, deduction_quiz_page, culture_quiz_page, creativty_quiz_page, reasoning_quiz_page, technical_quiz_page, quiz_results], position="hidden")
+
 pages.run()
