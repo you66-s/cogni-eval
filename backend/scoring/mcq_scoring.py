8a3c0bc4091730c0e0efb9d358042633e532c324
youssef <youssef@email.com>
2025-08-21 18:38:54 -0700

    full implementation of the MCQ question scoring class

--- backend/scoring/mcq_scoring.py
+++ backend/scoring/mcq_scoring.py
@@ -1,11 +1,20 @@
 import pandas as pd
 
 class MCQScoring:
-    def __init__(self, dataset, question_index, user_answers):
+    def __init__(self, dataset: pd.DataFrame, question_index: list, user_answers: list, dimension: str):
         self.__dataset = dataset
-        self.__questions = question_index
-        self.__answers = user_answers
-        self.__correct_answers = None
+        self.__questions_index = question_index
+        self.__user_answers = user_answers
     
-    def get_questions(self):
-        
+    def get_correct_answers(self) -> list:
+        self.__answers = [] * len(self.__questions_index)
+        for i in self.__questions_index:
+            self.__answers.append(self.__dataset['answer_index'].iloc[i])
+        return self.__answers
+
+    def evaluate_quiz(self) -> int:
+        points = 0
+        for index in range(len(self.__user_answers)):
+            if self.__user_answers[index] == self.__answers[index]:
+                points += 1
+        return points
