d077fc8cceefe45294c3efe13e6794c2188e65d2
youssef <youssef@email.com>
2025-08-23 18:39:25 -0700

    simplifing the class by just using basic list to calculate the score

--- backend/scoring/mcq_scoring.py
+++ backend/scoring/mcq_scoring.py
@@ -1,20 +1,13 @@
 import pandas as pd
 
 class MCQScoring:
-    def __init__(self, dataset: pd.DataFrame, question_index: list, user_answers: list, dimension: str):
-        self.__dataset = dataset
-        self.__questions_index = question_index
-        self.__user_answers = user_answers
-    
-    def get_correct_answers(self) -> list:
-        self.__answers = [] * len(self.__questions_index)
-        for i in self.__questions_index:
-            self.__answers.append(self.__dataset['answer_index'].iloc[i])
-        return self.__answers
+    def __init__(self, correct_answer: list, user_answer: list):
+        self.__correct_answer = correct_answer
+        self.__user_answer = user_answer
 
     def evaluate_quiz(self) -> int:
         points = 0
-        for index in range(len(self.__user_answers)):
-            if self.__user_answers[index] == self.__answers[index]:
+        for index in range(len(self.__user_answer)):
+            if self.__correct_answer[index] == self.__user_answer[index]:
                 points += 1
         return points
