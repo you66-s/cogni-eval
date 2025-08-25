9cfbeae63e49cfbd5c5ad43900617482af78f925
youssef <youssef@email.com>
2025-08-24 20:51:56 -0700

    major fix: refactor of code to separate the logic of the code from the page structure

--- UI/pages/creativity_quiz_page.py
+++ UI/pages/creativity_quiz_page.py
@@ -1,45 +1,23 @@
 import streamlit as st
 import pandas as pd
 import random
-import time
+
+from backend.utils.functions import quiz_generation_and_scoring
 
 # Dataset import
 dataset = pd.read_csv('../Data/final_dataset/final_dataset.csv')
 creativity_dim = dataset[dataset['dimension'] == "Creativity"]
-dataset_length = len(creativity_dim)
+
 st.write("<h1 style='text-align: center;'>Creativity Section</h1>", unsafe_allow_html=True)
 # Initialize quiz only once
 if "creativity_quiz" not in st.session_state:
-    questions_index = random.sample(range(dataset_length), 5)
+    questions_index = random.sample(range(len(creativity_dim)), 5)
     st.session_state.creativity_quiz = {
         "questions_index": questions_index,
         "user_answer": [""] * 5,
-        "current_q": 0  # step tracker
+        "current_q": 0
     }
-
 quiz = st.session_state.creativity_quiz
 current = quiz["current_q"]
 
-# Show current question
-if current >= len(quiz["questions_index"]):
-    st.success("Quiz finished 🎉")
-    time.sleep(5)
-    st.switch_page("pages/culture_quiz_page.py")
-else:
-    # Get current question
-    question_index = quiz["questions_index"][current]
-
-    st.write(f"<h3>Question {current + 1}</h3>", unsafe_allow_html=True)
-    st.write(creativity_dim['question'].iloc[question_index])
-
-    user_answer = st.text_area(
-        "Write your answer here:",
-        key=f"c_q{current}",
-        value=quiz["user_answer"][current]
-    )
-
-    if st.button("Next"):
-        # Save answer
-        quiz["user_answer"][current] = user_answer
-        quiz["current_q"] += 1
-        st.rerun()
+quiz_generation_and_scoring(current, quiz, creativity_dim, "open-ended", "pages/culture_quiz_page.py", "Creativity")
--- UI/pages/culture_quiz_page.py
+++ UI/pages/culture_quiz_page.py
@@ -1,14 +1,8 @@
 import streamlit as st
 import pandas as pd
 import random
-import numpy as np
-import time
+from backend.utils.functions import quiz_generation_and_scoring
 
-def mcq_choices_parser(choices) -> list:
-    choices_dict = eval(choices, {"array": np.array})
-    choices = list(choices_dict['text'])
-    labels = list(choices_dict['label'])
-    return choices, labels
 
 # Dataset
 dataset = pd.read_csv('../Data/final_dataset/final_dataset.csv')
@@ -29,29 +23,4 @@ if "culture_quiz" not in st.session_state:
 quiz = st.session_state.culture_quiz
 current = quiz["current_q"]
 
-# Stop if quiz finished
-if current >= len(quiz["questions_index"]):
-    st.success("Quiz finished 🎉")
-    time.sleep(5)
-    st.switch_page("pages/deduction_quiz_page.py")
-else:
-    # Get current question
-    question_index = quiz["questions_index"][current]
-
-    st.write(f"<h3>Question {current + 1}</h3>", unsafe_allow_html=True)
-    st.write(culture_dim['question'].iloc[question_index])
-
-    choices, labels = mcq_choices_parser(culture_dim['choices'].iloc[question_index])
-
-    # Use the actual question_index in key → unique per question
-    selected = st.radio(
-        "Select the answer",
-        options=choices,
-        key=f"qst_{question_index}"
-    )
-
-    if st.button("Next"):
-        # Save answer
-        quiz["user_answer"][current] = choices.index(selected) if selected else None
-        quiz["current_q"] += 1
-        st.rerun()
+quiz_generation_and_scoring(current, quiz, culture_dim, culture_dim["question_type"].unique()[0], "pages/deduction_quiz_page.py", culture_dim["dimension"].unique()[0])
