f073673724eb8825b6ef9fa0ede1700df9dd948a
youssef <youssef@email.com>
2025-08-25 11:45:46 -0700

    removing the mcq parser function and using plain list strings for choices

--- backend/utils/functions.py
+++ backend/utils/functions.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from backend.scoring.scoring_engine import ScoringEngine
 import numpy as np
+import time
 
 def mcq_choices_parser(choices) -> list:
     choices_dict = eval(choices, {"array": np.array})
@@ -24,12 +25,11 @@ def quiz_generation_and_scoring(current, quiz, dataset: pd.DataFrame, qst_type: 
     
     # Show current question
     if current >= len(quiz["questions_index"]):
+        print(f"{dimension} section answers")
+        print(f"user answers {quiz["score_user_answer"]}")
+        print(f"correct answers {quiz["score_crr_answer"]}")
         scoring = ScoringEngine("open-ended", quiz["score_user_answer"], quiz["score_crr_answer"])
-        if "score" not in quiz:
-            quiz['score'] = []
-            quiz['score'].append({dimension: scoring.evaluate_quiz()})
-        else:
-            quiz['score'].append({dimension: scoring.evaluate_quiz()})
+        st.session_state['score'].append({dimension: scoring.evaluate_quiz()})
 
         # Clearing the scoring arrays for the next quiz
         quiz["score_user_answer"] = []
@@ -38,6 +38,10 @@ def quiz_generation_and_scoring(current, quiz, dataset: pd.DataFrame, qst_type: 
     else:
         # Get current question
         question_index = quiz["questions_index"][current]
+        #DEBUG
+        st.write("Correct answer index:", dataset["answer_index"].iloc[question_index])
+        st.write("Choices:", dataset["choices"].iloc[question_index])
+        #---------------------------END OF DEBUG----------------------------------
         st.write(f"<h3>Question {current + 1}</h3>", unsafe_allow_html=True)
         st.write(dataset['question'].iloc[question_index])
 
@@ -56,7 +60,7 @@ def quiz_generation_and_scoring(current, quiz, dataset: pd.DataFrame, qst_type: 
                 quiz["current_q"] += 1
                 st.rerun()
         elif qst_type == "MCQ":
-            choices, labels = mcq_choices_parser(dataset['choices'].iloc[question_index])
+            choices = list(eval(dataset['choices'].iloc[question_index]))
 
             # Use the actual question_index in key 
             selected = st.radio(
@@ -64,7 +68,6 @@ def quiz_generation_and_scoring(current, quiz, dataset: pd.DataFrame, qst_type: 
                 options=choices,
                 key=f"qst_{question_index}"
             )
-
             if st.button("Next"):
                 # Save answer
                 quiz["user_answer"][current] = choices.index(selected) if selected else None
