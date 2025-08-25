b1ce6fe466aef741a110a7f752cab5c80ade3787
youssef <youssef@email.com>
2025-08-24 20:50:35 -0700

    minor fix: renaming of the variables for a significant names

--- backend/scoring/scoring_engine.py
+++ backend/scoring/scoring_engine.py
@@ -3,16 +3,17 @@ from backend.scoring.mcq_scoring import MCQScoring
 import pandas as pd
 
 class ScoringEngine:
-    def __init__(self, question_type, user_answer=None, ref_answer=None):
+    def __init__(self, question_type, user_answer: list, correct_answer: list):
         self.__question_type = question_type
         self.__user_answer = user_answer
-        self.__ref_answer = ref_answer
-        self.__open_ended_similarity = OpenEndedScoring(user_answers=self.__user_answer, ref_answers=self.__ref_answer)
-        self.__mcq_score = MCQScoring(user_answer=self.__user_answer, correct_answer=self.__ref_answer)
+        self.__correct_answer = correct_answer
+        self.__open_ended_similarity = OpenEndedScoring(self.__user_answer, self.__correct_answer)
+        self.__mcq_score = MCQScoring(self.__user_answer, self.__correct_answer)
 
     def evaluate_quiz(self):
         if self.__question_type == "MCQ":
             return self.__mcq_score.evaluate_quiz()
         elif self.__question_type == "open-ended":
-            self.__open_ended_similarity.calculate_similarity()
+            similartiy = self.__open_ended_similarity.calculate_similarity()
+            print("similarity calculated: ", similartiy)
             return self.__open_ended_similarity.final_point_calculator()
