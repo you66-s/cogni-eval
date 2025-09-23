bb66e6f73c6e1ac1f1882939654419e44925cbb3
youssef <youssef@email.com>
2025-09-23 13:25:06 -0700

    update the scoring function

--- backend/scoring/scoring_engine.py
+++ backend/scoring/scoring_engine.py
@@ -2,17 +2,16 @@ from backend.scoring.open_answer_scoring import OpenEndedScoring
 from backend.scoring.mcq_scoring import MCQScoring
 
 class ScoringEngine:
-    def __init__(self, question_type, user_answer, correct_answer):
+    def __init__(self, question_type, user_answer, correct_answer, question=None):
+        self.__question = question
         self.__question_type = question_type
         self.__user_answer = user_answer
         self.__correct_answer = correct_answer
-        self.__open_ended_similarity = OpenEndedScoring(self.__user_answer, self.__correct_answer)
+        self.__open_ended_socre = OpenEndedScoring(self.__question, self.__user_answer, self.__correct_answer)
         self.__mcq_score = MCQScoring(self.__user_answer, self.__correct_answer)
 
     def evaluate_quiz(self):
         if self.__question_type == "MCQ":
             return self.__mcq_score.evaluate_quiz()
         elif self.__question_type == "open-ended":
-            similartiy = self.__open_ended_similarity.calculate_similarity()
-            print("similarity calculated: ", similartiy)
-            return self.__open_ended_similarity.final_point_calculator()
+            return self.__open_ended_socre.final_point_calculator()
